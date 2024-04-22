d01a5168d8e4aca89fcbadbe09de781491b43c59
jovita <jovitaamelinda05@gmail.com>
2024-04-22 09:02:54 +0800

    revisi exit window

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,16 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "695IDJZawWc":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  function closeWin() {
+myWindow.close();
+}
 }
 
